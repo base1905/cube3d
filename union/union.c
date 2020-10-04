96c78eb622b0c2bdb986a550bd3356f17df9325f
arannara <base1905@yandex.ru>
2020-10-04 18:28:40 +0300

    union home

--- union/union.c
+++ union/union.c
@@ -6,20 +6,11 @@
 /*   By: arannara <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 20:50:10 by arannara          #+#    #+#             */
-/*   Updated: 2020/10/03 23:59:06 by arannara         ###   ########.fr       */
+/*   Updated: 2020/10/04 18:10:19 by df               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-
-int issymbol(int a)
-{
-	if ((a >= 33) && (a <= 126))
-		return 1;
-	else
-		return 0;
-}
 
 int ft_strlen(char *s)
 {
@@ -29,48 +20,65 @@ int ft_strlen(char *s)
 	while (s[i])
 		i++;
 	return(i);
-
 }
 
-int before ( char b )
+void ft_putstr(char *str)
 {
-	if 
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
+int check(char *str, int len, char check)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (str[i] == check)
+			return (0);
+		i++;
+	}
+	return(1);
+}
 
 int main(int argc, char *argv[])
 {
 	int i;
 	char *str;
 	int len;
-	int count;
 	int j;
 
+	str = NULL;
 	if (argc <= 2 || argc > 3)
 		write(1, "\n", 1);
 	else
 	{
-		i = 0;
-		count = 0;
-
-		str = argv[1];
-		len = ft_strlen(argv[1]);
-
-		while (++i < len)
+		i = 1;
+		len = 0;
+		while (argv[i])
 		{	
 			j = 0;
-			while (++j < len)
+			while (argv[i][j] != '\0')
 			{
-				if (str[i] == str[j] && i != j) 
-					count++;
+				if (check(str, len, argv[i][j]))
+				{
+					str[len] = argv[i][j];
+					len++;
+				}
+				j++;
 			}
-			if (count > 0)
-				write (1, &str[i], 1);
-			count = 0;
-
+			i++;
 		}
-		printf("\nargv2 = %d", ft_strlen(argv[2]));
+		str[len] = '\0';
+		ft_putstr(str);
 	}
 	return(0);
 }
-https://github.com/alanbarrett2/42-Final-Exam/tree/master/2-0-union
+//https://github.com/alanbarrett2/42-Final-Exam/tree/master/2-0-union
